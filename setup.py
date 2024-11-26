1357aa6362f63a65e021d3660682b76ed1279693
Hassan <hassan.foroughi@gmail.com>
2024-11-26 15:31:50 +0000

    chore: read version from version.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+# Read the version from version.txt
+with open('version.txt') as version_file:
+    version = version_file.read().strip()
+
 setup(
     name='geofetch',
-    version='1.0.0',  # Update this as needed
+    version=version,
     description='A simplistic command-line tool to fetch GEO data based on user input.',
     author='Hassan Foroughi',
     packages=find_packages(),
