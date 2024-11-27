9ea95812949827eca522448da18b3bcec8ae8fb4
Hassan <hassan.foroughi@gmail.com>
2024-11-27 09:49:50 +0000

    feat!: add multiple items to sample entry and add a function for building query

--- geo_query/geo_query.py
+++ geo_query/geo_query.py
@@ -12,55 +12,41 @@ def generate_random_email():
     return f"{username}@{domain}"
 
 
+def build_query(terms, query_type, operator='OR'):
+    query = []
+    operator = f' {operator} '
+    if terms:
+        for term in terms:
+            query.append(f'{term}[{query_type}]')
+        return f'({operator.join(query)})'
+    return ''
+
+
 @click.command()
-@click.option('-t', '--title', multiple=True, help='Title(s) of the study or dataset.')
-@click.option('-d', '--description', multiple=True, help='Description(s) of the study or dataset.')
 @click.option('-o', '--organism', default='Homo sapiens', help='Organism name (default: Homo sapiens).')
-@click.option('-m', '--mesh', multiple=True, default=['Diabetes Mellitus, Type 2'], help='Medical Subject Headings (MeSH) terms.')
 @click.option('-ds', '--date-start', default='2000/01/01', help='Start date for the data collection (format: YYYY/MM/DD).')
 @click.option('-de', '--date-end', default='3000', help='End date for the data collection (format: YYYY/MM/DD).')
-@click.option('-e', '--entry', default='gds', type=click.Choice(['gds', 'gpl', 'gse', 'gsm']), help='Type of entry (gds, gpl, gse, gsm).')
-@click.option('-s', '--sample', default='rna', type=click.Choice(['rna', 'protein', 'genomic']), help='Type of sample (rna, protein, genomic).')
+@click.option('-e', '--entry', default='gse', type=click.Choice(['gds', 'gpl', 'gse', 'gsm']), help='Type of entry (gds, gpl, gse, gsm).')
+@click.option('-m', '--mesh', multiple=True, default=['Diabetes Mellitus, Type 2'], help='Medical Subject Headings (MeSH) terms.')
+@click.option('-s', '--sample', multiple=True, default=['rna'], type=click.Choice(['rna', 'mpss', 'sage', 'protein', 'genomic']), help='Type of sample.')
+@click.option('-t', '--title', multiple=True, help='Title(s) of the study or dataset.')
+@click.option('-d', '--description', multiple=True, help='Description(s) of the study or dataset.')
 
 def cli(title, description, organism, mesh, date_start, date_end, sample, entry):
     """Fetch GEO data based on user input."""
 
     search_term = []
 
-
-    if sample:
-        search_term.append(f'{sample}[Sample Type]')
-
-    if entry:
-        search_term.append(f'{entry}[Entry Type]')
-
-    if organism:
-        search_term.append(f'{organism}[Organism]')
-
-    mesh_query= []
-    if mesh:
-        for m in mesh:
-            mesh_query.append(f'{m}[MeSH Terms]')
-        mesh_query = ' OR '.join(mesh_query)
-        search_term.append(f'({mesh_query})')
-        
-    if date_start and date_end:
-        search_term.append(f'({date_start}[Publication Date] : {date_end}[Publication Date])')
-        
-    description_query = []
+    search_term.append(f'{entry}[Entry Type]')
+    search_term.append(f'{organism}[Organism]')
+    search_term.append(f'({date_start}[Publication Date] : {date_end}[Publication Date])')
+    search_term.append(build_query(terms=sample, query_type='Sample Type', operator='OR'))
+    search_term.append(build_query(terms=mesh, query_type='MeSH Terms', operator='OR'))
     if description:
-        for d in description:
-            description_query.append(f'{d}[Description]')
-        description_query = ' OR '.join(description_query)
-        search_term.append(f'({description_query})')
-        
-    title_query = []
+        search_term.append(build_query(terms=description, query_type='Description', operator='OR'))
     if title:
-        for t in title:
-            title_query.append(f'{t}[Title]')
-        title_query = ' OR '.join(title_query)
-        title_query = f'({title_query})'
-        
+        search_term.append(build_query(terms=title, query_type='Title', operator='OR'))
+
     search_term = ' AND '.join(search_term)
 
     Entrez.email = generate_random_email()
@@ -68,12 +54,12 @@ def cli(title, description, organism, mesh, date_start, date_end, sample, entry)
     print(f"Using email: {Entrez.email}")
     print(f"Query: {search_term}")
     
-    handle = Entrez.esearch(db="gds", term=search_term)
-    record = Entrez.read(handle)
-    handle.close()
+#    handle = Entrez.esearch(db="gds", term=search_term)
+#    record = Entrez.read(handle)
+#    handle.close()
 #    print(record)
-    print(f"Valid query: {record['QueryTranslation']}")
-    print(f"Found {record['Count']} {entry} for above query.")
+#    print(f"Valid query: {record['QueryTranslation']}")
+#    print(f"Found {record['Count']} {entry} for above query.")
 
 if __name__ == "__main__":
     cli()
